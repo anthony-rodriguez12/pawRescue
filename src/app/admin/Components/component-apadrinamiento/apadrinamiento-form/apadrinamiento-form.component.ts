5ce4fdbd67bf1817227fbc530f42f011d6466301
Proyect-Ati <atiheras@gmail.com>
2024-01-21 23:21:37 -0500

    Apadrinamiento 70%

--- src/app/admin/Components/component-apadrinamiento/apadrinamiento-form/apadrinamiento-form.component.ts
+++ src/app/admin/Components/component-apadrinamiento/apadrinamiento-form/apadrinamiento-form.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+// pet-form.component.ts
+import { Component, Input, OnChanges, SimpleChanges, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import { finalize } from 'rxjs/operators';
+import { EditApadrinamiento } from 'src/app/client/services/apadrinamiento.service';
+import { PetService } from 'src/app/client/services/pet.service';
+export interface State {
+  idEstado: number
+  estadoDesc: string
+  adopciones: any[]
+  animales: any[]
+  apadrinamientos: any[]
+  rescates: any[]
+  usuarios: any[]
+  voluntarios: any[]
+}
 
 @Component({
   selector: 'app-apadrinamiento-form',
   templateUrl: './apadrinamiento-form.component.html',
   styleUrls: ['./apadrinamiento-form.component.scss']
 })
-export class ApadrinamientoFormComponent {
+export class ApadrinamientoFormComponent implements OnInit, OnChanges {
+  @Input() formData: any;
+  @Input() IdData: any;
+
+  @Output() editedDataEmitter = new EventEmitter<any>();
+  myForm!: FormGroup;
+  animaldata: any[] = []
+  edit: boolean = false
+  stateanimal: State[] = []
+
+  constructor(private fb: FormBuilder,
+    private animalService: PetService,
+  ) { }
+
+  ngOnInit(): void {
+    this.createform();
+    this.myForm.valueChanges.subscribe(() => {
+      this.editedDataEmitter.emit(this.myForm.value);
+    });
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['formData']) {
+      let dataadopcion : EditApadrinamiento = changes['formData'].currentValue;
+      this.getApadrinamiento(dataadopcion)
+    }
+  }
+
+  createform() {
+    this.myForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      direccion: ['', Validators.required],
+      fechaCreacion: ['', Validators.required],
+      telefono: ['', Validators.required],
+      correo: ['', Validators.required],
+      idEstudios: ['', Validators.required],
+      motivo: ['', Validators.required],
+      idAnimal: ['', Validators.required],
+      nameAnimal: ['', Validators.required],
+      idEstado: ['', Validators.required],
+      monto: ['', Validators.required],
+      idPeriodicidad: ['', Validators.required]
+    });
+  }
+
+
+  setRegiste(data: any) {
+    if (data) {
+      const FindAnimal = this.animaldata.find(animal => animal.idAnimal === data.idAnimal);
+      this.myForm.get('idAnimal')?.setValue(data.idAnimal);
+      this.myForm.get('nameAnimal')?.setValue(FindAnimal.nombre);
+    }
+  }
+
+  setdata(data: EditApadrinamiento) {
+    if (data) {
+      this.edit = true;
+      const today = moment();
+      const FindAnimal = this.animaldata.find(animal => animal.idAnimal === data.idAnimal);
+      this.myForm.get('fechaCreacion')?.setValue(today.format('YYYY-MM-DD'));
+      this.myForm.get('nombre')?.setValue(data.nombre);
+      this.myForm.get('apellido')?.setValue(data.apellido);
+      this.myForm.get('direccion')?.setValue(data.direccion);
+      this.myForm.get('telefono')?.setValue(data.telefono);
+      this.myForm.get('correo')?.setValue(data.correo);
+      this.myForm.get('motivo')?.setValue(data.motivo);
+      this.myForm.get('monto')?.setValue(data.monto);
+      this.myForm.get('idAnimal')?.setValue(data.idAnimal);
+      this.myForm.get('nameAnimal')?.setValue(FindAnimal.nombre);
+      this.myForm.get('idPeriodicidad')?.setValue(data.idPeriodicidad);
+
+    } else {
+      this.edit = false;
+    }
+
+  }
+
+  getApadrinamiento(dataadopcion?: any): void {
+    this.animalService.getAnimals().pipe(
+      finalize(() => {
+        this.setdata(dataadopcion);
+      })
+    ).subscribe((res) => {
+      this.animaldata = res.data;
+    });
+  }
+
 
 }
--- src/app/admin/Components/component-apadrinamiento/pages/apadrinamiento-list/apadrinamiento-list.component.ts
+++ src/app/admin/Components/component-apadrinamiento/pages/apadrinamiento-list/apadrinamiento-list.component.ts
@@ -1,10 +1,152 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { PetService } from 'src/app/client/services/pet.service';
+import { TooltipPosition } from '@angular/material/tooltip';
+import { SnackbarService } from 'src/app/shared/services/snackbar.service';
+import { PetAddComponent } from '../../../component-animal/pages/pet-add/pet-add.component';
+import { PetEditComponent } from '../../../component-animal/pages/pet-edit/pet-edit.component';
+import { ApadrinamientoService } from 'src/app/client/services/apadrinamiento.service';
+import { ApadrinamientoEditComponent } from '../apadrinamiento-edit/apadrinamiento-edit.component';
+
 
 @Component({
   selector: 'app-apadrinamiento-list',
   templateUrl: './apadrinamiento-list.component.html',
   styleUrls: ['./apadrinamiento-list.component.scss']
 })
-export class ApadrinamientoListComponent {
+export class ApadrinamientoListComponent
+  implements AfterViewInit {
+  displayedColumns: string[] = ['acction', 'nombre', 'apadrinado', 'direccion', 'correo', 'telefono'];
+  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
 
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  nombreFilter: string = "";
+  animalFilter: string = "";
+
+  animals: any[] = [];
+
+  constructor(private animalService: PetService,
+    public dialog: MatDialog,
+    private _snackBar: SnackbarService,
+    private ServiceApadrinamiento: ApadrinamientoService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAnimals();
+    this.getAllApadriamiento();
+  }
+
+  getAllApadriamiento(): void {
+    this.ServiceApadrinamiento.getAllApadrinamiento().subscribe((res) => {
+      console.log("res.data", res.data);
+      this.dataSource.data = res.data;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  getAnimalsbyID(id: number): string {
+    const estado = this.animals.find((animal) => animal.idAnimal === id);
+    return estado ? estado.nombre : 'Desconocido';
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(column?: string): void {
+    this.dataSource.filterPredicate = (data, filter) => {
+      const filterObject = JSON.parse(filter);
+      for (const key in filterObject) {
+        if (filterObject.hasOwnProperty(key)) {
+          const columnValue = String(data[key]).toLowerCase();
+          const filterValue = String(filterObject[key]).toLowerCase();
+          if (!columnValue.includes(filterValue)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+    this.dataSource.filter = this.createFilter();
+  }
+
+  createFilter(): string {
+    const animal = this.animals.find((animal) => animal.nombre.includes(this.animalFilter));
+    const filters = {
+      nombre: this.nombreFilter,
+      idAnimal: animal ? animal.idAnimal : this.animalFilter,
+    };
+    const validFilters = Object.entries(filters)
+      .filter(([key, value]) => value !== undefined && value !== null && value !== '')
+      .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+    return JSON.stringify(validFilters);
+  }
+
+  getAnimals(): void {
+    this.animalService.getAnimals().subscribe((res) => {
+      this.animals = res.data;
+    });
+  }
+  // Editar Animal
+  editAnimal(element: any): void {
+    const dialogRef: MatDialogRef<ApadrinamientoEditComponent> = this.dialog.open(ApadrinamientoEditComponent, {
+      data: element.idApadrinamiento,
+      width: '800px',
+      maxHeight: '750px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === '200') {
+        this._snackBar.sucess('Aviso', 'Registro editado correctamente.');
+        this.getAnimals();
+      } else if (result === '500') {
+        this._snackBar.danger('Error', 'Oops! Algo salió mal al intentar editar el registro. Por favor, inténtalo de nuevo.');
+      }
+    });
+  }
+  // Agregar Animal
+  addAnimal(): void {
+    const dialogRef: MatDialogRef<PetAddComponent> = this.dialog.open(PetAddComponent, {
+      width: '800px',
+      maxHeight: '700px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === '201') {
+        this._snackBar.sucess('Aviso', 'Registro guardado correctamente.');
+        this.getAnimals();
+      } else {
+        this._snackBar.danger('Error', 'Oops! Algo salió mal al intentar agregar el registro. Por favor, inténtalo de nuevo.');
+      }
+    });
+  }
+
+}
+
+
+
+export class CustomMatPaginatorIntl extends MatPaginatorIntl {
+  override itemsPerPageLabel = 'Elementos por página';
+  override nextPageLabel = 'Siguiente página';
+  override previousPageLabel = 'Página anterior';
+  override firstPageLabel = 'Primera página';
+  override lastPageLabel = 'Última página';
+
+  override getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `0 de ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} de ${length}`;
+  };
 }
